329ec788aa04b6305af54dd1d045b8958fd7d990
satmika ozora <mikaozora08@gmail.com>
2024-08-11 17:03:49 +0700

    fix: bug

--- app/src/main/java/com/example/axforasset/PageDetail.java
+++ app/src/main/java/com/example/axforasset/PageDetail.java
@@ -4,11 +4,13 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.pdf.PdfDocument;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -22,6 +24,8 @@ import android.widget.TextView;
 
 public class PageDetail extends AppCompatActivity {
 
+    String username;
+    boolean state = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +33,10 @@ public class PageDetail extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Intent i = getIntent();
+        username = i.getStringExtra("username");
+        System.out.println(username);
 
         // set the value
         Intent iDetail = getIntent();
@@ -61,7 +69,9 @@ public class PageDetail extends AppCompatActivity {
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
+                Intent iHome = new Intent(PageDetail.this, HomeActivity.class);
+                iHome.putExtra("username", username);
+                startActivity(iHome);
             }
         });
 
@@ -83,22 +93,38 @@ public class PageDetail extends AppCompatActivity {
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                submitButton.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.btn_clicked));
+
+                // Revert to the original color after a delay (e.g., 1 second)
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        // Change back to the original color
+                        submitButton.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.primary));
+                    }
+                }, 1000);
                 // Ambil nama dari EditText
                 String name = editTextName.getText().toString();
                 AlertDialog.Builder builder = new AlertDialog.Builder(PageDetail.this);
                 if (name.isEmpty()) {
                     builder.setTitle("Submission Failed");
                     builder.setMessage("Please fill in your email address");
-                } else if (paymentMethodSpinner.getSelectedItem().toString().equals("Choose Payment")) {
+                } else if (paymentMethodSpinner.getSelectedItem().toString().equals("Payment Method")) {
                     builder.setTitle("Submission Failed");
-                    builder.setMessage("Please choose your payment method");
+                    builder.setMessage("Please choose payment method");
                 } else {
+                    state = true;
                     builder.setTitle("Submission Success");
                     builder.setMessage("A confirmation email has been sent to your provided email address. Please check your inbox");
                 }
+                Intent iHome = new Intent(PageDetail.this, HomeActivity.class);
+                iHome.putExtra("username", username);
                 builder.setPositiveButton("CLOSE", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+                        if(state){
+                            startActivity(iHome);
+                        }
                         dialog.dismiss();
                     }
                 });
@@ -121,11 +147,13 @@ public class PageDetail extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.home:
                 Intent iHome = new Intent(PageDetail.this, HomeActivity.class);
+                iHome.putExtra("username", username);
                 startActivity(iHome);
                 break;
 
             case R.id.profile:
                 Intent iProfile = new Intent(PageDetail.this, ProfileActivity.class);
+                iProfile.putExtra("username", username);
                 startActivity(iProfile);
                 break;
 
